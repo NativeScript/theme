8ccd3013bb44d1511d9898af2e9296f581f5aa2f
Josh Sommer <JoshDSommer@gmail.com>
2016-09-02 14:00:50 -0400

    creating actionBar component
    Reusable action bar for all the pages

---
+++ app/actionBar/actionBar.ts
@@ -0,0 +1,20 @@
+import {Observable} from 'data/observable';
+import {topmost} from 'ui/frame';
+import { EventData } from "data/observable";
+import { Page } from "ui/page";
+import { Label } from 'ui/label';
+import { NavigationViewModel } from '../navigation-vm';
+
+interface ICustomActionBar extends Page{
+	ActionBarTitle: string;
+}
+
+export function onLoad(args: EventData) {
+	let page = <ICustomActionBar>args.object;
+	console.log(page.ActionBarTitle);
+	let actionBarTitle = <Label>page.getViewById("actionBarTitle");
+
+	actionBarTitle.text = page.ActionBarTitle;
+
+	page.bindingContext = new NavigationViewModel(page);
+}
