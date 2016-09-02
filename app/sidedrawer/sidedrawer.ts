441e826f38f8d8747f569d520ccc2813945e61a6
Josh Sommer <JoshDSommer@gmail.com>
2016-09-01 22:34:50 -0400

    adding buttons active to side drawer

--- app/sidedrawer/sidedrawer.ts
+++ app/sidedrawer/sidedrawer.ts
@@ -4,124 +4,132 @@ import { EventData } from "data/observable";
 import { Page } from "ui/page";
 
 export function onLoad(args: EventData) {
-  let page = <Page>args.object;
-  page.bindingContext = new SideDrawer();
+	let page = <Page>args.object;
+	page.bindingContext = new SideDrawer();
 }
 export class SideDrawer extends Observable {
 
-  constructor() {
-    super();
-  }
+	constructor() {
+		super();
+	}
 
-  public viewHome() {
+	public viewHome() {
 		let navigationEntry = {
 			moduleName: 'main-page',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 
-  public viewBasics() {
+	public viewBasics() {
 		let navigationEntry = {
 			moduleName: 'pages/basics',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 
-  public viewButtons() {
-   	let navigationEntry = {
+	public viewButtons() {
+		let navigationEntry = {
 			moduleName: 'pages/buttons',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 
-  public viewForms() {
+	public viewButtonsActive() {
+		let navigationEntry = {
+			moduleName: 'pages/buttons-active',
+			clearHistory: true
+		};
+		topmost().navigate(navigationEntry);
+	}
+
+	public viewForms() {
 		let navigationEntry = {
 			moduleName: 'pages/forms',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 
-  public viewSliders() {
+	public viewSliders() {
 		let navigationEntry = {
 			moduleName: 'pages/sliders',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 
-  public viewSwitches() {
+	public viewSwitches() {
 		let navigationEntry = {
 			moduleName: 'pages/switches',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 
-  public viewProgress() {
+	public viewProgress() {
 		let navigationEntry = {
 			moduleName: 'pages/progress',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 
-  public viewSearch() {
+	public viewSearch() {
 		let navigationEntry = {
 			moduleName: 'pages/search',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 
-  public viewTabs() {
+	public viewTabs() {
 		let navigationEntry = {
 			moduleName: 'pages/tabs',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 
-  public viewSegmentBar() {
+	public viewSegmentBar() {
 		let navigationEntry = {
 			moduleName: 'pages/segmentbar',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 
-  public viewDialogs() {
+	public viewDialogs() {
 		let navigationEntry = {
 			moduleName: 'pages/dialogs',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 
-  public viewListView() {
+	public viewListView() {
 		let navigationEntry = {
 			moduleName: 'pages/listview',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 
-  public viewSideDrawer() {
+	public viewSideDrawer() {
 		let navigationEntry = {
 			moduleName: 'pages/sidedrawer',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 
-  public viewThemes() {
+	public viewThemes() {
 		let navigationEntry = {
 			moduleName: 'pages/themes',
 			clearHistory: true
 		};
 		topmost().navigate(navigationEntry);
-  }
+	}
 }
