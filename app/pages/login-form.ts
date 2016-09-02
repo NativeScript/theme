18c3302bb821351f9bc9ba75087f384b969bd4b9
Ryan Lebel <ryan@firescript.ca>
2016-09-02 14:43:16 -0400

    login

---
+++ app/pages/login-form.ts
@@ -0,0 +1,42 @@
+import { EventData } from "data/observable";
+import { Page } from "ui/page";
+import {Observable} from 'data/observable';
+import * as frameModule from 'ui/frame';
+import {Color} from "color";
+import {topmost} from 'ui/frame';
+
+export class ProgressModel extends Observable {
+
+  constructor() {
+    super();
+  }
+}
+
+export function navigatingTo(args: EventData) {
+    var page = <Page>args.object;
+    page.bindingContext = new ProgressModel();
+}
+
+export function loaded(args){
+  var page = <Page>args.object;
+  
+    if (page.ios) {
+        var navigationBar = frameModule.topmost().ios.controller.navigationBar;        
+        navigationBar.translucent = true;
+        navigationBar.barStyle = 1;  
+        page.backgroundSpanUnderStatusBar = true; 
+        page.actionBarHidden = true;
+        var grey = new Color('#4A90E2');
+        navigationBar.tintColor = grey.ios;         
+        // navigationBar.shadowImage = new UIImage();
+        // navigationBar.setBackgroundImageForBarMetrics(new UIImage(), UIBarMetrics.UIBarMetricsDefault)
+    }  
+}
+
+export function goBack(){
+    topmost().navigate({
+        moduleName: "pages/login-landing",
+        clearHistory: true,
+        animated: false
+    });   
+}
