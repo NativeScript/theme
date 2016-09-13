ff63f5754dbfcba670d82e7da35cc3ad79146afe
Nathan Walker <walkerrunpdx@gmail.com>
2016-09-13 10:50:06 -0700

    chore: removed unneeded demo code

--- app/sidedrawer/sidedrawer.ts
+++ app/sidedrawer/sidedrawer.ts
@@ -25,97 +25,9 @@ export function onLoad(args: EventData) {
 
 export class SideDrawerViewModel extends NavigationViewModel {
   public static activeComponent: string;
-	private activeItem = 'sidedrawer-list-item active';
-	private inactiveItem = 'sidedrawer-list-item';
 
 	constructor(public page: Page, private activeComponent: string) {
     super(page);
     SideDrawerViewModel.activeComponent = activeComponent;
-	}
-	public isHomeActive() {
-		if (this.activeComponent == 'home') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
-	}
-
-	public isBasicsActive() {
-		if (this.activeComponent == 'basics') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
-	}
-
-	public isButtonsActive() {
-		if (this.activeComponent == 'buttons') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
-	}
-	public isButtonsActiveActive() {
-		if (this.activeComponent == 'buttons-active') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
-	}
-	public isDialogsActive() {
-		if (this.activeComponent == 'dialogs') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
-	}
-	public isFormsActive() {
-		if (this.activeComponent == 'forms') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
-	}
-	public isListActive() {
-		if (this.activeComponent == 'list') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
-	}
-	public isProgressActive() {
-		if (this.activeComponent == 'progress') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
-	}
-	public isSegmentActive() {
-		if (this.activeComponent == 'segmentbar') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
-	}
-	public isSearchActive() {
-		if (this.activeComponent == 'search') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
-	}
-	public isSlidersActive() {
-		if (this.activeComponent == 'sliders') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
-	}
-	public isSwitchesActive() {
-		if (this.activeComponent == 'switches') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
-	}
-	public isTabsActive() {
-		if (this.activeComponent == 'tabs') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
-	}
-	public isThemesActive() {
-		if (this.activeComponent == 'theme') {
-			return this.activeItem;
-		}
-		return this.inactiveItem;
 	}
 }
