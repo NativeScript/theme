997b3366c5f4b52dfe1c7e7362ac6a920211fa85
Nathan Walker <walkerrunpdx@gmail.com>
2016-11-26 22:44:45 -0800

    feat(Colors): add color classes for color/background-color with demo page

---
+++ app/pages/colors.ts
@@ -0,0 +1,12 @@
+import { EventData } from "data/observable";
+import { Page } from "ui/page";
+import { BaseModel } from './base';
+
+export class ColorsModel extends BaseModel {
+
+}
+
+export function navigatingTo(args: EventData) {
+  var page = <Page>args.object;
+  page.bindingContext = new ColorsModel(page);
+}
