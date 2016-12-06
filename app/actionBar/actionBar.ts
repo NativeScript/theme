ce9db897ac7adf61d6782384f7d15d56d464c1f1
Nathan Walker <walkerrunpdx@gmail.com>
2016-12-05 20:09:29 -0800

    fix: forms spacing when same controls are used in a vertical stack

--- app/actionBar/actionBar.ts
+++ app/actionBar/actionBar.ts
@@ -1,16 +1,15 @@
-import {Observable} from 'data/observable';
-import {topmost} from 'ui/frame';
+import { Observable } from 'data/observable';
+import { topmost } from 'ui/frame';
 import { EventData } from "data/observable";
 import { Page } from "ui/page";
 import { Label } from 'ui/label';
-import { NavigationViewModel } from '../navigation-vm';
 
-interface ICustomActionBar extends Page{
-	ActionBarTitle: string;
+interface ICustomActionBar extends Page {
+  ActionBarTitle: string;
 }
 
 export function onLoad(args: EventData) {
-	let page = <ICustomActionBar>args.object;
+  let page = <ICustomActionBar>args.object;
   let actionBarTitle = <Label>page.getViewById("actionBarTitle");
   actionBarTitle.text = page.ActionBarTitle;
 }
