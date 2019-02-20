eb32e447666a7a8e68c502fadc4a6402bedaf07b
Bundyo <bundyo@gmail.com>
2019-02-20 11:06:06 +0200

    Update ThemeIcon to support icon fonts Cleanup

--- app/app.js
+++ app/app.js
@@ -4,22 +4,22 @@ You can use this file to perform app-level initialization, but the primary
 purpose of the file is to pass control to the app’s first module.
 */
 import * as app from "tns-core-modules/application";
-import { TNSFontIcon, fonticon } from "nativescript-fonticon";
+//import { TNSFontIcon, fonticon } from "nativescript-fonticon";
 // import { isActive,capitalize } from "./app-root/app-root";
 import "nativescript-dom";
 
-TNSFontIcon.debug = false;
-TNSFontIcon.paths = {
-    "fa": "assets/css/font-awesome.css"
-};
-TNSFontIcon.loadCss();
+// TNSFontIcon.debug = false;
+// TNSFontIcon.paths = {
+//     "fa": "assets/css/font-awesome.css"
+// };
+// TNSFontIcon.loadCss();
 
-const resources = app.getResources();
-resources["fonticon"] = fonticon;
+//const resources = app.getResources();
+//resources["fonticon"] = fonticon;
 // resources["isActive"] = isActive;
 // resources["capitalize"] = capitalize;
 
-app.setResources(resources);
+//app.setResources(resources);
 
 require("./bundle-config");
 
--- src/ThemeIcon/ThemeIcon.js
+++ src/ThemeIcon/ThemeIcon.js
@@ -1,20 +1,59 @@
 import { Property } from "tns-core-modules/ui/core/view";
 import { Label } from "tns-core-modules/ui/label";
 
+const updateIcon = (target) => target.updateIcon();
+
 export const nameProperty = new Property({
     name: "name",
     defaultValue: undefined,
+    valueChanged: updateIcon
+});
+
+export const nsProperty = new Property({
+    name: "ns",
+    defaultValue: "fa",
     valueChanged: (target, oldValue, newValue) => {
-        target.text = newValue;
+        target.classList.remove(oldValue);
+        target.classList.add(newValue);
     }
+});
+
+export const variantProperty = new Property({
+    name: "variant",
+    defaultValue: "fa",
+    valueChanged: updateIcon
 });
 
 export class ThemeIcon extends Label {
     constructor() {
         super();
 
-        this.className = "theme-menu__icon fa";
+        this.className = "theme-menu__icon";
+    }
+
+    updateIcon() {
+        if (!this._styleScope) {
+            return;
+        }
+
+        this._styleScope._selectors.class[`${this.variant}-${this.name}`]
+            .flatMap((value) => value.sel.ruleset.declarations)
+            .reverse()
+            .some((dec) => {
+                if (dec.content) {
+                    this.text = dec.content;
+                    return true;
+                }
+            });
+    }
+
+    onLoaded() {
+        super.onLoaded();
+
+        this.updateIcon();
     }
 }
 
+nsProperty.register(ThemeIcon);
 nameProperty.register(ThemeIcon);
+variantProperty.register(ThemeIcon);
