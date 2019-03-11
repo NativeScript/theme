4f20963609de402b6c2f3719d47668e76a0fb8a2
Kamen Bundev <bundyo@gmail.com>
2019-03-11 18:41:40 +0200

    Fix the ActionBar

--- src/ThemeActionBar/ThemeActionBar.js
+++ src/ThemeActionBar/ThemeActionBar.js
@@ -5,39 +5,42 @@ import template from "./ThemeActionBar.template.xml";
 import { isAndroid } from "tns-core-modules/platform";
 
 export const titleProperty = new Property({name: "title", defaultValue: undefined});
-export const menuProperty = new Property({name: "menu", defaultValue: false});
-export const backButtonProperty = new Property({name: "backButton", defaultValue: false});
 
 export class ThemeActionBar extends ActionBar {
+    static get menuEvent() { return "menu" }
+    static get backEvent() { return "back" }
+
     constructor() {
         super();
 
         let component = parse(template);
         component.bindingContext = this;
 
-        this.eachChild(() => {
-            debugger;
-        });
-
         this.className = "theme-action-bar";
         this.titleView = component;
 
         if (isAndroid) {
-            this.once('loaded', ({object}) => {
+            this.once('loaded', ({ object }) => {
                 object.nativeViewProtected.setContentInsetsAbsolute(0, 0);
-
-                this.eachChild(() => {
-                    debugger;
-                });
             });
         }
     }
 
-    onTap(args) {
-        this.itemSelected(this.items.getItem(args.index), args);
+    _addChildFromBuilder() {
+        //debugger;
+    }
+
+    onTap(event, args) {
+        this.notify(Object.assign(args,{ eventName: event }));
+    }
+
+    onMenuTap(args) {
+        this.onTap("menu", args);
+    }
+
+    onBackTap(args) {
+        this.onTap("back", args);
     }
 }
 
 titleProperty.register(ThemeActionBar);
-menuProperty.register(ThemeActionBar);
-backButtonProperty.register(ThemeActionBar);
