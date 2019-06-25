8864bcbf02b01aa405d58c8b6fec1c616d85bee1
Kamen Bundev <bundyo@gmail.com>
2019-06-25 14:44:19 +0300

    Add pickers, fix their light and dark themes, add Kendo support

--- app/pages/basics.js
+++ app/pages/basics.js
@@ -1,9 +1,6 @@
 import { BaseModel } from "./base";
 
-export class BasicsModel extends BaseModel {
-
-}
-
+export class BasicsModel extends BaseModel {}
 
 export function navigatingTo(args) {
     var page = args.object;
