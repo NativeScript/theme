f35e0d4099eff1ce2243ad926d4d031cfac11853
Kamen Bundev <bundyo@gmail.com>
2019-07-05 18:43:58 +0300

    Remove {N}-theme-widgets completely  (should fix the icons and navigation)

--- app/app.js
+++ app/app.js
@@ -6,6 +6,37 @@ purpose of the file is to pass control to the app’s first module.
 import * as app from "tns-core-modules/application";
 import "nativescript-theme-core";
 
+export function fonticon(value) {
+    if (value) {
+        const scope = app.getRootView()._styleScope;
+
+        if (!scope) {
+            return;
+        }
+
+        const icon = scope._selectors.class[value];
+
+        // eslint-disable-next-line no-unused-expressions
+        icon && icon
+            .flatMap((value) => value.sel.ruleset.declarations)
+            .reverse()
+            .some((dec) => {
+                if (dec.property === "content") {
+                    const char = dec.value.replace(/^"|"$/g, "");
+
+                    value = char.startsWith("\\") ?
+                        String.fromCharCode(`0x${(char.match(/[a-f\d]{2,4}/i) || [])[0]}`) :
+                        char;
+
+                    return true;
+                }
+            });
+    }
+
+    return value;
+}
+
+app.setResources({ fonticon });
 app.run({ moduleName: "app-root/app-root" });
 
 /*
